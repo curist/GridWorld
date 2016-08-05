dbbe19aaf9a1225d3e4313479782a370a1f64d2f
neil <neil@ghtinc.com>
2016-08-05 13:08:06 +0800

    auto traverse

--- app/views/GridWorld.js
+++ app/views/GridWorld.js
@@ -10,6 +10,7 @@ import './gridworld.less';
 
 const ALPHA = 0.2;
 const GAMMA = 0.8;
+const EPSILON = 0.8;
 const M = 10;
 const N = 10;
 
@@ -29,6 +30,8 @@ const GridWorld = {
 
     ctrl.current_m = m.prop(0);
     ctrl.current_n = m.prop(0);
+
+    ctrl.showQvalue = m.prop(false);
 
     // available directions
     const directions = {
@@ -72,6 +75,12 @@ const GridWorld = {
     };
     ctrl.resetStartPosition();
 
+    ctrl.reachedEndState = () => {
+      const m = ctrl.current_m();
+      const n = ctrl.current_n();
+      return ctrl.maze[m][n] > 50;
+    };
+
     ctrl.handleKeyUp = (e) => {
       const { key } = e;
       const direction = key.toLowerCase().replace('arrow', '');
@@ -87,21 +96,82 @@ const GridWorld = {
       window.removeEventListener('keydown', ctrl.handleKeyUp);
     };
 
+    ctrl.newMaze = () => {
+      ctrl.maze = generateMaze(M, N);
+      reset();
+    };
+
+    ctrl.toggleShowQvalue = () => {
+      ctrl.showQvalue(!ctrl.showQvalue());
+    };
+
+    function traverse() {
+      if(ctrl.reachedEndState()) {
+        ctrl.resetStartPosition();
+        return;
+      }
+      const dirs = Object.keys(directions);
+      if(Math.random() < EPSILON) {
+        // take a random move
+        const dir = dirs[Math.floor(Math.random() * dirs.length)];
+        handleAction(dir);
+        return;
+      }
+      const m = ctrl.current_m();
+      const n = ctrl.current_n();
+      let bestDir, bestDirValue = -Infinity;
+      dirs.forEach(dir => {
+        const q = Q(m, n, dir);
+        if(q > bestDirValue) {
+          bestDirValue = q;
+          bestDir = dir;
+        }
+      });
+      handleAction(bestDir);
+    }
+
+    ctrl.interval = null;
+    ctrl.toggleTraverse = () => {
+      if(ctrl.interval) {
+        clearInterval(ctrl.interval);
+        ctrl.interval = null;
+        return;
+      }
+      ctrl.interval = setInterval(() => {
+        traverse();
+        m.redraw();
+      }, 30);
+    };
+
   },
   view (ctrl) {
     const current_m = ctrl.current_m();
     const current_n = ctrl.current_n();
     return m('.GridWorld', [
-      m('button',  {
+      m('button', {
+        onclick: ctrl.newMaze.bind(ctrl),
+      }, 'new maze'),
+      m('button', {
+        onclick: ctrl.toggleTraverse.bind(ctrl),
+      }, (function() {
+        if(ctrl.interval === null) {
+          return 'start auto traverse';
+        } else {
+          return 'stop auto traverse';
+        }
+      })()),
+      m('button', {
         onclick: reset,
       }, 'reset q table'),
+      m('button', {
+        onclick: ctrl.toggleShowQvalue.bind(ctrl),
+      }, 'toggle show q values'),
       m('.maze', ctrl.maze.map((row, x) => {
         return m('.row', row.map((grid, y) => {
           let current = '';
-          if(x == current_m && y == current_n) {
+          if(ctrl.showQvalue() || x == current_m && y == current_n) {
             current = '.current';
           }
-          let text = (grid == 0 ? '' : grid);
           // values
           const tvalue = Q(x, y, 'up');
           const tcolor = valueToColor(tvalue);
@@ -118,17 +188,17 @@ const GridWorld = {
           }, [
             m('.triangle.top', {
               style: `border-top-color:${tcolor}`
-            }, m('span', tvalue)),
+            }, m('span', tvalue || '')),
             m('.triangle.right', {
               style: `border-right-color:${rcolor}`
-            }, m('span', rvalue)),
+            }, m('span', rvalue || '')),
             m('.triangle.bottom', {
               style: `border-bottom-color:${bcolor}`
-            }, m('span', bvalue)),
+            }, m('span', bvalue || '')),
             m('.triangle.left', {
               style: `border-left-color:${lcolor}`
-            }, m('span', lvalue)),
-            m('.text', text),
+            }, m('span', lvalue || '')),
+            m('.text', grid || ''),
           ]);
         }));
       })),
